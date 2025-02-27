51a0a470de82024d14989f2a1cb83c00ed01bf4d
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-02-28 00:29:11 +0200

    added a function to count word in a sentence

--- FCS62-2nd-homework-dictionaries.py
+++ FCS62-2nd-homework-dictionaries.py
@@ -25,3 +25,14 @@ print(merge_dictionaries(dict1, dict2))
 
 # Expected Output:
 # {"apple": 3, "orange": 2, "banana": 1}
+example_2 = "apple orange banana apple orange apple"
+def count_words(sentence):
+    words = sentence.split()
+    word_count = {}
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+print(count_words(example_2))
