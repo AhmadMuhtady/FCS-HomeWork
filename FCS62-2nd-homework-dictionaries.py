66d6d7d8ea954ef3ab26c3d7c108cf6b3c4c67f5
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-02-28 00:53:03 +0200

    added part 3 dictionary structure

--- FCS62-2nd-homework-dictionaries.py
+++ FCS62-2nd-homework-dictionaries.py
@@ -36,3 +36,21 @@ def count_words(sentence):
             word_count[word] = 1
     return word_count
 print(count_words(example_2))
+
+# Create a dictionary named that stores information about employees in different departments.
+
+# Example:
+# company_employees = {
+# "Engineering": {
+# "Alice": {"age": 30, "role": "Software Engineer"},
+# "Bob": {"age": 28, "role": "DevOps Engineer"}
+# },
+# "HR": {
+# "Charlie": {"age": 35, "role": "HR Manager"}
+# }
+# }
+
+
+# a) Print .
+# b) Add a new employee "David" in "Engineering" with role "Data Scientist" and age 27.
+# c) Write a function to count the total number of employees in the company.
