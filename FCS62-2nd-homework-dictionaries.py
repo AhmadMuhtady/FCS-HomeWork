643fd9fd85513d0fc39af40482dd198aca549fb0
AhmadMuhtady <ahmad.d.muhtadi@gmail.com>
2025-02-27 23:43:47 +0200

    added part two count word

--- FCS62-2nd-homework-dictionaries.py
+++ FCS62-2nd-homework-dictionaries.py
@@ -17,4 +17,11 @@ def merge_dictionaries(dict1, dict2):
     return {**dict1, **dict2}
 print(merge_dictionaries(dict1, dict2))
 
+# 2 - Write a program that takes a sentence as input and prints a dictionary where:
+# are words in the sentence.
+# are the number of times each word appears.
+# Example:
+# "apple orange banana apple orange apple"
 
+# Expected Output:
+# {"apple": 3, "orange": 2, "banana": 1}
